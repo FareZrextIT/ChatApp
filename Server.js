b86b5ee41815c77fb9a73b717619c5364f2c7cea
FareZrextIT <faris.milic11@gmail.com>
2025-05-21 21:22:40 +0200

    Dodan Server.js i node_modules u .gitignore

---
+++ Server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const http =
+    require("http").createServer(app);
+const io = require("socket.io")(
+    http,
+);
+
+app.get("/",(req, res)=> {
+    res.sendFile(
+        _dirname + "/public/index.html"
+    );
+
+});
+
+io.on("connection" , (socket)=> {
+    console.log("A user coonected");
+
+    socket.on("disconnect", ()=> {
+        console.log(
+            "A user disconnected",
+        );
+    });
+
+    socket.on(
+      "chat message",
+      (msg)=> {
+        console.log("message: " + msg);
+        io.emit("chat message", msg);
+
+      },
+    
+    );
+
+});
+
+http.listen(3000, ()=>{
+    console.log(
+        "Server started on port 3000",
+    );
+
+});
+
+
+
